6f5556fa54884a397150e421766eb50b09130486
Krzysztof Spalinski <krzysztof@spalinski.eu>
2021-11-01 20:30:07 +0100

    feat: add plot method to Chromosome class

--- cknots/analysis/chromosome.py
+++ cknots/analysis/chromosome.py
@@ -4,7 +4,11 @@ import re
 from dataclasses import dataclass, field
 from typing import List
 
+from matplotlib import pyplot as plt
+from matplotlib.collections import PolyCollection
+
 from cknots.analysis.ccd import CCD
+from cknots.analysis.link import Link
 
 
 @dataclass
@@ -24,7 +28,7 @@ class Chromosome:
             link_count += len(ccd.links)
         return link_count
 
-    def load_from_file(self, path, name=None):
+    def load_from_path(self, path, name=None):
         """
         path: path to directory with cKNOTs results for given chromosome.
         chromosome_number: 1 to 22 or 'X' or 'Y'
@@ -57,6 +61,57 @@ class Chromosome:
 
             ccd.load_from_file(os.path.join(path, ccd_results['results_filename']))
             self.ccds.append(ccd)
+
+    def plot(self):
+
+        rectangles = []
+        for link in self.get_links():
+            r = [
+                (link.min(), 0),
+                (link.min(), 1),
+                (link.max(), 1),
+                (link.max(), 0),
+                (link.min(), 0)
+            ]
+            rectangles.append(r)
+
+        links = PolyCollection(rectangles, facecolors='red', zorder=2)
+
+        rectangles = []
+        for ccd in self.ccds:
+            r = [
+                (ccd.start, 0),
+                (ccd.start, 1),
+                (ccd.end, 1),
+                (ccd.end, 0),
+                (ccd.start, 0)
+            ]
+            rectangles.append(r)
+
+        ccds = PolyCollection(rectangles, facecolors='cyan', zorder=0)
+
+        plt.figure(figsize=(16, 3))
+        ax = plt.gca()
+        ax.add_collection(links)
+        ax.add_collection(ccds)
+
+        ax.autoscale()
+        ax.get_yaxis().set_ticks([])
+
+        plt.title(f"Chromosome {self.name}")
+        plt.xlabel("Location (in relation to HG38 reference genome)")
+
+        plt.show()
+
+    def get_links(self) -> List[Link]:
+        links = []
+        for ccd in self.ccds:
+            links = links + ccd.links
+
+        return links
+
+    def __get_links_locations(self):
+        return [{'min': x.min(), 'max': x.max()} for x in self.get_links()]
 
     def __str__(self):
         return f'Chromosome {self.name} with {len(self.ccds)} CCDs containing {self.count_links()} links.'
